f6a17b04a8c9c0b6d3519c48ba65c9a23dd612e6
Fred Bessette <msfredb7@gmail.com>
2017-02-24 18:45:51 -0500

    Pathfinding ! + character

---
+++ GameJam2017/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MyAIPath))]
+public class Character : MonoBehaviour {
+
+
+    void Awake()
+    {
+        pather = GetComponent<MyAIPath>();
+    }
+
+    #region Movement
+
+    private MyAIPath pather;
+
+    public void MoveTo(Vector2 position)
+    {
+        pather.SetTarget(new Vector3(position.x, 0, position.y));
+    }
+
+    public void Stop()
+    {
+        pather.Stop();
+    }
+
+    public void LookTarget(Transform target)
+    {
+        pather.lookTarget = target;
+    }
+
+    #endregion
+}
---
+++ GameJam2017/Assets/Test&Examples/Fred/FredTestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FredTestScript : MonoBehaviour {
+    public MyAIPath character;
+    public Transform waypointA;
+    public Transform waypointB;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            character.SetTarget(waypointA.position);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            character.SetTarget(waypointB.position);
+        }
+    }
+}
