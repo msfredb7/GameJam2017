98c341c6ac4a5518415bb3a3f700c5397790b30d
DESKTOP-A46VL7R\etata <fivocx@gmail.com>
2017-02-25 02:37:35 -0500

    Fin Vendredi

--- GameJam2017/Assets/Test&Examples/Benoit/Script/DisplayObject.cs
+++ GameJam2017/Assets/Test&Examples/Benoit/Script/DisplayObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class DisplayObject : MonoBehaviour {
@@ -14,35 +15,72 @@ public class DisplayObject : MonoBehaviour {
 
     public class objectContent
     {
-        public string objectName;
-        public List<List<string>> categoryContent = new List<List<string>>();
+        private string objectName;
+        private List<string> categoryContent = new List<string>();
     }
 
 
     public List<objectType> listObjets = new List<objectType>();
+
     private List<GameObject> categoryButton = new List<GameObject>();
-    private List<Text> categoryText = new List<Text>();
+    private List<GameObject> textZones = new List<GameObject>();
 
     public GameObject categoryTabButton;
     public GameObject tabLayoutGroup;
     public GameObject contentLayoutGroup;
+    public GameObject textZone;
+
+    public ClickCategoryTab clickCatTab;
 
     private int currentObjectType;
-
     public Text objetName;
+
     void Start()
     {
+        clickCatTab.tabClicked.AddListener(changeTab);
+
         //currentCategory = 0;
         //categoryText.text = categoryName[nbCategory];
     }
 
-    public void changeObjectType()
+    public void changeTab(int tab)
     {
-        destroyOldButtons();
-        instanciateNewButtons();
+        destrotTextZones();
+        instanciateTextZones();
     }
 
-    public void destroyOldButtons()
+    public void destrotTextZones()
+    {
+        while (textZones.Count > 0)
+        {
+            Destroy(textZones[textZones.Count - 1]);
+            categoryButton.RemoveAt(textZones.Count - 1);
+        }
+    }
+
+    public void instanciateTextZones()
+    {
+        int nbTextZones = listObjets[currentObjectType].categoryName.Count;
+        for (int i = 0; i < nbCategory; i++)
+        {
+            GameObject newButton = Instantiate(categoryTabButton);
+            Text newText = newButton.GetComponentInChildren<Text>();
+            newButton.GetComponent<ClickCategoryTab>().numeroButton = i;
+            newButton.transform.SetParent(tabLayoutGroup.transform);
+            newText.text = listObjets[currentObjectType].categoryName[i];
+
+            categoryButton.Add(newButton);
+        }
+    }
+
+
+    public void changeObjectType()
+    {
+        destroyButtons();
+        instanciateButtons();
+    }
+
+    public void destroyButtons()
     { 
         while (categoryButton.Count > 0)
         {
@@ -51,14 +89,14 @@ public class DisplayObject : MonoBehaviour {
         }
     }
 
-    public void instanciateNewButtons()
+    public void instanciateButtons()
     {
         int nbCategory = listObjets[currentObjectType].categoryName.Count;
         for (int i = 0; i < nbCategory; i++ )
         {
             GameObject newButton = Instantiate(categoryTabButton);
             Text newText = newButton.GetComponentInChildren<Text>();
-
+            newButton.GetComponent<ClickCategoryTab>().numeroButton = i;
             newButton.transform.SetParent(tabLayoutGroup.transform);
             newText.text = listObjets[currentObjectType].categoryName[i];
 
