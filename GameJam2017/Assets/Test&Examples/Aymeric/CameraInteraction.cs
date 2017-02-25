bdbdf0157e0107038fab1b8b7105800c329dcbb3
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-02-24 22:23:32 -0500

    Follow NPC and update selection camera

--- GameJam2017/Assets/Test&Examples/Aymeric/CameraInteraction.cs
+++ GameJam2017/Assets/Test&Examples/Aymeric/CameraInteraction.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class CameraInteraction : MonoBehaviour {
+
+    enum CameraState {fix, following};
 
     public GameObject IUCamera1;
     public GameObject Camera1;
+    private CameraState state1 = CameraState.fix;
 
     public GameObject IUCamera2;
     public GameObject Camera2;
+    private CameraState state2 = CameraState.fix;
 
     public Color selectedColor;
     public Color unSelectedColor;
 
+
+    #region hoveredCamera
+
+    int hoveredCamera;
+
+    public void enter1()
+    {
+        hoveredCamera = 1;
+    }
+
+    public void enter2()
+    {
+        hoveredCamera = 2;
+    }
+
+    public void exit1()
+    {
+        if(hoveredCamera!=2)
+        {
+            hoveredCamera = 0;
+        }
+    }
+
+    public void exit2()
+    {
+        if(hoveredCamera!=1)
+        {
+            hoveredCamera = 0;
+        }
+    }
+
+    #endregion
 
     #region SelectionCamera
     //La camera actuellement séléctioné
@@ -99,22 +137,22 @@ public class CameraInteraction : MonoBehaviour {
     {
         if (Input.GetAxis("Vertical") < 0)
         {
-            if(selectedCamera == 1)
+            if(selectedCamera == 1 && state1 != CameraState.following)
             {
                 Camera1.transform.Translate(new Vector3(0, -0.1f, 0));
             }
-            else if(selectedCamera == 2)
+            else if(selectedCamera == 2 && state2 != CameraState.following)
             {
                 Camera2.transform.Translate(new Vector3(0, -0.1f, 0));
             }
         }
         if (Input.GetAxis("Vertical") > 0)
         {
-            if (selectedCamera == 1)
+            if (selectedCamera == 1 && state1 != CameraState.following)
             {
                 Camera1.transform.Translate(new Vector3(0, 0.1f, 0));
             }
-            else if (selectedCamera == 2)
+            else if (selectedCamera == 2 && state2 != CameraState.following)
             {
                 Camera2.transform.Translate(new Vector3(0, 0.1f, 0));
             }
@@ -122,32 +160,90 @@ public class CameraInteraction : MonoBehaviour {
 
         if (Input.GetAxis("Horizontal") < 0)
         {
-            if (selectedCamera == 1)
+            if (selectedCamera == 1 && state1 != CameraState.following)
             {
                 Camera1.transform.Translate(new Vector3(-0.1f, 0, 0));
             }
-            else if (selectedCamera == 2)
+            else if (selectedCamera == 2 && state2 != CameraState.following)
             {
                 Camera2.transform.Translate(new Vector3(-0.1f, 0, 0));
             }
         }
         if (Input.GetAxis("Horizontal") > 0)
         {
-            if (selectedCamera == 1)
+            if (selectedCamera == 1 && state1 != CameraState.following)
             {
                 Camera1.transform.Translate(new Vector3(0.1f, 0, 0));
             }
-            else if (selectedCamera == 2)
+            else if (selectedCamera == 2 && state2 != CameraState.following)
             {
                 Camera2.transform.Translate(new Vector3(0.1f, 0, 0));
             }
         }
     }
-        
+
+    #endregion
+
+    #region ClickOnNPC
+
+    private void OnMouseDown()
+    {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(hoveredCamera != selectedCamera)
+            {
+                selectedCamera = hoveredCamera;
+            }
+            else
+            {
+                if (selectedCamera == 1)
+                {
+                    Ray ray = Camera1.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.collider.GetComponent<TempoTestMouvement>() != null)
+                        {
+                            Camera1.GetComponent<FollowObject>().follow(hit.collider.transform);
+                            state1 = CameraState.following;
+                        }
+                        else
+                        {
+                            Camera1.GetComponent<FollowObject>().stopFollow();
+                            state1 = CameraState.fix;
+                        }
+                    }
+                }
+
+                if (selectedCamera == 2)
+                {
+                    Ray ray = Camera2.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        if (hit.collider.GetComponent<TempoTestMouvement>() != null)
+                        {
+                            Camera2.GetComponent<FollowObject>().follow(hit.collider.transform);
+                            state2 = CameraState.following;
+                        }
+                        else
+                        {
+                            Camera2.GetComponent<FollowObject>().stopFollow();
+                            state2 = CameraState.fix;
+                        }
+                    }
+                }
+            }
+        }
+            
+    }
+
     #endregion
 
     public void Update()
     {
         mouvementCamera();
+        OnMouseDown();
     }
 }
