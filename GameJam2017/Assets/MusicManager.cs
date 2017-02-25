df6d67a2c1bd5af1eb3fb376f22d7c02a296dc1e
Xeal <alexandre-blanchet94@hotmail.com>
2017-02-25 16:20:07 -0500

    Ajout de la musique et fix pour Eric

---
+++ GameJam2017/Assets/MusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CCC.Manager;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource audio;
+
+    void Start()
+    {
+        DelayManager.CallTo(delegate()
+        {
+            audio.Play();
+        },9);
+        
+    }
+}
