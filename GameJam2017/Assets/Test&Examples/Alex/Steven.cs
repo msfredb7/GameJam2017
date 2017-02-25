ae9abe89a275b577ea293965c87d5c0e47dc05f8
Xeal <alexandre-blanchet94@hotmail.com>
2017-02-25 15:02:54 -0500

    Prefab et personnages

---
+++ GameJam2017/Assets/Test&Examples/Alex/Steven.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steven : Brain {
+
+    public override void ToDo()
+    {
+        // Tout les evennements qu'il devra faire
+    }
+}
