bdded3ccc9ac2b08407f3c8aab7e2e68193dd891
DESKTOP-A46VL7R\etata <fivocx@gmail.com>
2017-02-25 01:26:19 -0500

    Premier commit (UI and some scripts)

---
+++ GameJam2017/Assets/Test&Examples/Benoit/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public Sprite sprite;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void callSpawner()
+    {
+        SpriteSpawner.instance.SpawnSprite(sprite, new Vector2(10,0), 20);
+    }   
+
+
+}
