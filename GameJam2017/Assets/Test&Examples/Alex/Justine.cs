c549d29ca64b66af619339025dc889531dd2c69f
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-02-25 22:38:21 -0500

    Justine déplacements

--- GameJam2017/Assets/Test&Examples/Alex/Justine.cs
+++ GameJam2017/Assets/Test&Examples/Alex/Justine.cs
@@ -6,6 +6,36 @@ public class Justine : Brain {
 
     public override void ToDo()
     {
-        // Tout les evennements qu'il devra faire
+        //8H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(0, WayPoint.getWaypoint("JustineDesk").position, personnage));
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(28, WayPoint.getWaypoint("StevenDesk").position, personnage));
+        //9H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(60, WayPoint.getWaypoint("JustineDesk").position, personnage));
+
+        //10H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(150, WayPoint.getWaypoint("BossDesk").position, personnage));
+        //11H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(195, WayPoint.getWaypoint("JustineDesk").position, personnage));
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(230, WayPoint.getWaypoint("SalleDesEmployes").position, personnage));
+        //12H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(270, WayPoint.getWaypoint("WCRed").position, personnage));
+        //13H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(300, WayPoint.getWaypoint("PlacardConsierge").position, personnage));
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(330, WayPoint.getWaypoint("WCRed").position, personnage));
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(340, WayPoint.getWaypoint("JustineDesk").position, personnage));
+        //14H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(360, WayPoint.getWaypoint("SamanthaDesk").position, personnage));
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(390, WayPoint.getWaypoint("JustineDesk").position, personnage));
+        //15H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(435, WayPoint.getWaypoint("BossDesk").position, personnage));
+
+        //16H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(480, WayPoint.getWaypoint("JustineDesk").position, personnage));
+
+        //17H
+
+        //18H
+        ScenarioEventManager.AddEvent(new MoveScenarioEvent(615, WayPoint.getWaypoint("SalleReunionSud").position, personnage));
+
     }
 }
