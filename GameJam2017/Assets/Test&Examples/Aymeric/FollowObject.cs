0dc67b60b2366ef9a3efb7214d0fe20d979bad3a
Fred Bessette <msfredb7@gmail.com>
2017-02-26 01:23:12 -0500

    Camera smooth follow

--- GameJam2017/Assets/Test&Examples/Aymeric/FollowObject.cs
+++ GameJam2017/Assets/Test&Examples/Aymeric/FollowObject.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class FollowObject : MonoBehaviour {
 
     public Transform inFollow;
+    public float speed = 1;
 
 
     public void Update()
     {
         if(inFollow != null)
         {
-            transform.position = inFollow.position + new Vector3(0, 10, 0);
+            Vector3 target = new Vector3(inFollow.position.x, transform.position.y, inFollow.position.z);
+            Vector3 dir = target - transform.position;
+            if (dir.magnitude > 1)
+                dir.Normalize();
+
+            transform.position += dir * speed * Time.deltaTime;
         }
         
     }
