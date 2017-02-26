c1751f6acb8b19b24178db443a4c9f1e8b073cc5
Xeal <alexandre-blanchet94@hotmail.com>
2017-02-26 15:22:42 -0500

    ending

--- GameJam2017/Assets/EndGame.cs
+++ GameJam2017/Assets/EndGame.cs
@@ -9,6 +9,7 @@ public class EndGame : MonoBehaviour {
 
     public AudioSource winMusic;
     public GameObject button;
+    public GameObject text;
 
     void Start()
     {
@@ -24,7 +25,13 @@ public class EndGame : MonoBehaviour {
         {
             DelayManager.CallTo(delegate ()
             {
+                text.SetActive(true);
+
+            }, 2);
+            DelayManager.CallTo(delegate ()
+            {
                 button.SetActive(true);
+
             }, 5);
         }
     }
